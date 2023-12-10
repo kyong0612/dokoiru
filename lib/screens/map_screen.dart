7a2557a3f5fe3cfb7225ab039cbb6990cb66f4e1
kimkiyong <fenderst0612@gmail.com>
2023-12-10 21:06:20 +0900

    feat: map camera

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -12,6 +12,7 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  late GoogleMapController mapController;
   final CameraPosition _initialCameraPosition = const CameraPosition(
     target: LatLng(35.681236, 139.767125), // 東京駅
     zoom: 16.0,
@@ -30,6 +31,12 @@ class _MapScreenState extends State<MapScreen> {
   }
 
   @override
+  void dispose() {
+    mapController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -37,12 +44,30 @@ class _MapScreenState extends State<MapScreen> {
       ),
       body: GoogleMap(
         initialCameraPosition: _initialCameraPosition,
+        onMapCreated: (GoogleMapController controller) {
+          mapController = controller;
+        },
+        myLocationEnabled: true,
+        myLocationButtonEnabled: false,
       ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: () {},
-      //   tooltip: 'Increment',
-      //   child: const Icon(Icons.add),
-      // ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final Position position = await Geolocator.getCurrentPosition(
+            desiredAccuracy: LocationAccuracy.high,
+          );
+          print(position);
+
+          // 現在地を中心にカメラを移動
+          mapController.animateCamera(CameraUpdate.newCameraPosition(
+            CameraPosition(
+              target: LatLng(position.latitude, position.longitude),
+              zoom: 16.0,
+            ),
+          ));
+        },
+        tooltip: 'current position',
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
