b6cc7551f94bb4e380767f0eceab94f3a6d8b1db
kimkiyong <fenderst0612@gmail.com>
2023-12-10 19:17:11 +0900

    feat: location permission

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({
@@ -15,6 +16,18 @@ class _MapScreenState extends State<MapScreen> {
     target: LatLng(35.681236, 139.767125), // 東京駅
     zoom: 16.0,
   );
+
+  @override
+  void initState() {
+    // 位置情報が許可されていない時に許可をリクエストする
+    Future(() async {
+      LocationPermission permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.denied) {
+        await Geolocator.requestPermission();
+      }
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
